699ef8d71b79a3a9083c39c1015da64f573c1d4b
Reece Kalmar <ralmarkalmar@gmail.com>
2023-05-20 20:22:58 -0600

    Deleted default layout icons && added own logo
    r

--- rydu.gg/src/App.tsx
+++ rydu.gg/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,12 +7,15 @@ function App() {
       <header className="App-header">
 
         <div className="menu">
-          <img src="/hambDrop.png" className="Menu-Dropimg" alt="menu" />
-          </div>
-          <div className='Search-Menu'>
-            <input type="search" placeholder='Search Players or Champions' className='Search-Bar' />
-          </div>
-        
+          <img src="/Page_Icons/hambDrop.png" className="Menu-Dropimg" alt="menu" />
+        </div>
+        <div className='Search-Menu'>
+          <input type="search" placeholder='Search Players or Champions' className='Search-Bar' />
+        </div>
+        <div className='Home-Page-Link'>
+          <img src="/Page_Icons/Rydu smaller.png" alt="Homepage Button" className='Rydu-Header-Logo' />
+        </div>
+
       </header>
       <body className='Page-Body'>
 
