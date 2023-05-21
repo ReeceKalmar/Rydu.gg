6ffaf1c361def80ebd4d26113b79053afcfe1c4b
Reece Kalmar <ralmarkalmar@gmail.com>
2023-05-20 18:53:35 -0600

    Updated Header
    R

--- rydu.gg/src/App.tsx
+++ rydu.gg/src/App.tsx
@@ -6,24 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="Tool-bar">
+
         <div className="menu">
           <img src="/hambDrop.png" className="Menu-Dropimg" alt="menu" />
-        </div>
-      </div>
+          </div>
+          <div className='Search-Menu'>
+            <input type="search" placeholder='Search Players or Champions' className='Search-Bar' />
+          </div>
+        
+      </header>
+      <body className='Page-Body'>
+
+      </body>
     </div>
   );
 }
